2db7693a000ea44a24bcee9bba304aa364f5084d
Igor Macevic <informatikdrive@gmail.com>
2024-04-21 01:19:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -110,41 +110,57 @@ def close_position(symbol, amount):
         raise
 
 
+def close_partial_position(symbol, amount):
+    global current_buy_price_xeta
+    try:
+        session.place_order(
+            category="spot", symbol=symbol, side='sell', orderType="Market", qty=amount)
+        print(f"Sold 50%, Current Buy Price still: {current_buy_price_xeta}")
+    except Exception as e:
+        logger.error(f"Error in close_position: {str(e)}")
+        raise
+
+
 async def process_signal(symbol, action):
     global current_buy_price_xeta
     try:
         usdt_balance = get_wallet_balance("USDT")
         if usdt_balance > 3:
             rounded_down = math.floor(usdt_balance)
-            xeta_price_temp = get_current_price(symbol)
+            xeta_price_temp = (get_current_price(symbol)*0.9989)
             logger.info(f"{action} signal received for {symbol} with price: {xeta_price_temp}")
 
-            # Calculate the time to wait until the next 30-second candle closes
+            # Calculate the time to wait until the next 3-minute candle closes
             current_time = time.time()
-            next_candle_close = math.ceil(current_time / 30) * 30
-            time_to_wait = next_candle_close - current_time - 2
+            next_candle_close = math.ceil(current_time / 180) * 180
+            time_to_wait = next_candle_close - current_time - 5
 
             if time_to_wait > 0:
-                logger.info(f"Waiting {time_to_wait:.2f} seconds for the next 30-second candle close")
+                logger.info(f"Waiting {time_to_wait:.2f} seconds for the next 3-minute candle close")
                 for i in range(int(time_to_wait), 0, -1):
                     logger.info(f"Countdown: {i} seconds remaining")
                     await asyncio.sleep(1)
             else:
-                logger.info("Less than 5 seconds remaining for the next 30-second candle close. Proceeding with the check.")
+                logger.info("Less than 5 seconds remaining for the next 3-minute candle close. Proceeding with the check.")
 
             # Check if the current price meets the conditions based on the signal type
             current_price = get_current_price(symbol)
-            if action == "stillbuy":
-                if current_price <= xeta_price_temp:
-                    logger.info(
-                        f"Current price ({current_price}) is lower than the temp price ({xeta_price_temp}). Proceeding with the buy.")
+            if action == "buy":
+                if current_price >= xeta_price_temp:
+                    logger.info(f"Current price ({current_price}) is equal to or higher than the temp price ({xeta_price_temp}). Proceeding with the buy.")
                     open_position(symbol, rounded_down)
                     current_buy_price_xeta = get_current_price(symbol)
                 else:
-                    logger.info(
-                        f"Current price ({current_price}) is equal to or higher than the temp price ({xeta_price_temp}). Aborting the buy.")
+                    logger.info(f"Current price ({current_price}) is lower than the temp price ({xeta_price_temp}). Aborting the buy.")
                     current_buy_price_xeta = 0
-
+            elif action == "stillbuy":
+                if current_price < xeta_price_temp:
+                    logger.info(f"Current price ({current_price}) is lower than the temp price ({xeta_price_temp}). Proceeding with the buy.")
+                    open_position(symbol, rounded_down)
+                    current_buy_price_xeta = get_current_price(symbol)
+                else:
+                    logger.info(f"Current price ({current_price}) is equal to or higher than the temp price ({xeta_price_temp}). Aborting the buy.")
+                    current_buy_price_xeta = 0
         else:
             logger.info(f"Insufficient USDT balance to open a Buy position for {symbol}")
 
@@ -155,9 +171,12 @@ async def process_signal(symbol, action):
 
 async def check_price():
     global current_buy_price_xeta
-    stop_loss_threshold_percent = -0.5
-    take_profit_threshold_percent = 1.0
-    stop_loss_adjusted = False
+    initial_stop_loss_threshold_percent = -0.5
+    final_stop_loss_threshold_percent = - 1.5
+    sell_threshold_increments = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8]
+    stop_loss_threshold_percent = initial_stop_loss_threshold_percent
+    current_threshold_index = -1
+    initial_sell_triggered = False
     last_print_time = time.time()
     while True:
         if current_buy_price_xeta > 0:
@@ -170,20 +189,41 @@ async def check_price():
                     f'Buyprice: {current_buy_price_xeta} // Current Price: {current_price_xeta} // %-Change: {price_change_percent_xeta}')
                 last_print_time = current_time
 
-            if price_change_percent_xeta <= stop_loss_threshold_percent:
-                print(f"Price decreased by {price_change_percent_xeta:.2f}% for XETAUSDT. Selling all XETA.")
+            if not initial_sell_triggered and price_change_percent_xeta <= initial_stop_loss_threshold_percent:
+                print(f"Price decreased by {price_change_percent_xeta:.2f}% for XETAUSDT. Selling 50% of XETA.")
+                symbol_balance_xeta = get_wallet_balance("XETA")
+                if symbol_balance_xeta > 10:
+                    half_balance = math.floor(symbol_balance_xeta / 2)
+                    close_partial_position("XETAUSDT", half_balance)
+                    initial_sell_triggered = True
+
+            elif initial_sell_triggered and price_change_percent_xeta >= 0:
+                print(f"Price increased back to 0% for XETAUSDT. Rebuying XETA.")
+                usdt_balance = get_wallet_balance("USDT")
+                if usdt_balance > 3:
+                    rounded_down = math.floor(usdt_balance)
+                    open_position("XETAUSDT", rounded_down)
+                    initial_sell_triggered = False
+
+            elif initial_sell_triggered and price_change_percent_xeta <= final_stop_loss_threshold_percent:
+                print(
+                    f"Price decreased further by {price_change_percent_xeta:.2f}% for XETAUSDT. Selling remaining XETA.")
                 symbol_balance_xeta = get_wallet_balance("XETA")
                 if symbol_balance_xeta > 10:
                     symbol_balance_xeta = math.floor(symbol_balance_xeta)
                     close_position("XETAUSDT", symbol_balance_xeta)
 
-            elif price_change_percent_xeta >= 0.5 and not stop_loss_adjusted:
-                stop_loss_threshold_percent = 0.0
-                stop_loss_adjusted = True
-                print(f"Price increased by 0.5% for XETAUSDT. Adjusting stop loss to entry price.")
+            for i in range(len(sell_threshold_increments)):
+                if price_change_percent_xeta >= sell_threshold_increments[i] and i > current_threshold_index:
+                    current_threshold_index = i
+                    stop_loss_threshold_percent = sell_threshold_increments[i] - 0.5
+                    print(
+                        f"Price increased by {price_change_percent_xeta:.2f}% for XETAUSDT. Setting sell threshold to {stop_loss_threshold_percent:.2f}%.")
+                    break
 
-            elif price_change_percent_xeta >= take_profit_threshold_percent:
-                print(f"Price increased by {price_change_percent_xeta:.2f}% for XETAUSDT. Selling all XETA.")
+            if price_change_percent_xeta >= 10 or (
+                    price_change_percent_xeta <= stop_loss_threshold_percent and not initial_sell_triggered):
+                print(f"Price reached {price_change_percent_xeta:.2f}% for XETAUSDT. Selling all XETA.")
                 symbol_balance_xeta = get_wallet_balance("XETA")
                 if symbol_balance_xeta > 10:
                     symbol_balance_xeta = math.floor(symbol_balance_xeta)
